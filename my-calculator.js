a3bb96098c4eac92fd0e0ca4ecff16bc772a33dc
Seth Allgire <seth.allgire@gmail.com>
2021-09-20 08:44:56 -0500

    adds const displayValue

--- my-calculator.js
+++ my-calculator.js
@@ -1,12 +1,7 @@
 const buttons = document.querySelectorAll(".button");
-
+const displayValue = document.querySelector("#operand");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
-
-const output = document.querySelector("#operand");
-let screenDisplay = output.innerText;
-
-// const displayValue = document.querySelector("#operand").innerText;
 
 buttons.forEach(() => {
   addEventListener("click", updateDisplay);
@@ -15,11 +10,11 @@ buttons.forEach(() => {
 function updateDisplay(e) {
   if (e.target.classList.contains("number")) {
     let display = e.target.value;
-    document.querySelector("#operand").innerText += display;
+    displayValue.innerText += display;
     console.log(e.target.value);
   } else if (e.target.classList.contains("decimal-point")) {
     let display = e.target.value;
-    document.querySelector("#operand").innerText += display;
+    displayValue.innerText += display;
     console.log(e.target.value, "decimal-point");
   } else if (e.target.classList.contains("operator")) {
     console.log(e.target.value, "operator");

13578077d4f9e19d48de97af0afa78d7ef7cdc2e
Seth Allgire <seth.allgire@gmail.com>
2021-09-20 19:50:32 -0500

    adds second display and clear functions

--- my-calculator.js
+++ my-calculator.js
@@ -1,9 +1,12 @@
 const buttons = document.querySelectorAll(".button");
-const displayValue = document.querySelector("#operand");
+const displayValue = document.querySelector(".operand");
+const memoryDisplay = document.querySelector(".first-operand");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
+const clearEntry = document.querySelector(".clear-entry");
+const clearEverything = document.querySelector(".clear-all");
 
-buttons.forEach(() => {
+numberButtons.forEach(() => {
   addEventListener("click", updateDisplay);
 });
 
@@ -12,15 +15,36 @@ function updateDisplay(e) {
     let display = e.target.value;
     displayValue.innerText += display;
     console.log(e.target.value);
-  } else if (e.target.classList.contains("decimal-point")) {
-    let display = e.target.value;
-    displayValue.innerText += display;
-    console.log(e.target.value, "decimal-point");
-  } else if (e.target.classList.contains("operator")) {
-    console.log(e.target.value, "operator");
-  } else if (e.target.classList.contains("clear-entry")) {
-    console.log("clear-entry");
-  } else if (e.target.classList.contains("clear-all")) {
-    console.log("clear-all");
   }
 }
+
+clearEntry.addEventListener("click", clearDisplay);
+
+function clearDisplay(e) {
+  if (e.target.classList.contains("clear-entry")) {
+    displayValue.innerText = "";
+    console.log(e.target.value);
+  }
+}
+
+clearEverything.addEventListener("click", clearAll);
+
+function clearAll(e) {
+  if (e.target.classList.contains("clear-all")) {
+    displayValue.innerText = "";
+    memoryDisplay.innerText = "";
+    console.log(e.target.value);
+  }
+}
+
+// } else if (e.target.classList.contains("decimal-point")) {
+//   let display = e.target.value;
+//   displayValue.innerText += display;
+//   console.log(e.target.value, "decimal-point");
+// } else if (e.target.classList.contains("operator")) {
+//   console.log(e.target.value, "operator");
+// } else if (e.target.classList.contains("clear-entry")) {
+//   console.log("clear-entry");
+// } else if (e.target.classList.contains("clear-all")) {
+//   console.log("clear-all");
+// }

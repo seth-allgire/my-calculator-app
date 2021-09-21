826473aa8b0721bd816e653b2ac3f1a3e6be067f
Seth Allgire <seth.allgire@gmail.com>
2021-09-21 00:03:58 -0500

    adds switch statement to calculate

--- my-calculator.js
+++ my-calculator.js
@@ -1,13 +1,16 @@
 const buttons = document.querySelectorAll(".button");
 const displayValue = document.querySelector(".operand");
 const memoryDisplay = document.querySelector(".first-operand");
+const operationDisplay = document.querySelector(".operation");
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
 const calculateButton = document.querySelector(".equals");
 const clearEntry = document.querySelector(".clear-entry");
 const clearEverything = document.querySelector(".clear-all");
-const firstOperand = memoryDisplay.innerText;
-const secondOperand = displayValue.innerText;
+let mathOperator = "";
+let resultValue = "";
+let firstOperandValue = "";
+let secondOperandValue = "";
 
 numberButtons.forEach(() => {
   addEventListener("click", updateDisplay);
@@ -28,14 +31,47 @@ operatorButtons.forEach(() => {
 function operator(e) {
   if (
     e.target.classList.contains("operator") &&
-    displayValue.innerText !== "" &&
-    displayValue.innerText
+    displayValue.innerText !== ""
   ) {
-    let display = e.target.value;
-    displayValue.innerText += display;
+    mathOperator = e.target.value;
     memoryDisplay.innerText = displayValue.innerText;
+    operationDisplay.innerText += e.target.value;
     displayValue.innerText = "";
     console.log(e.target.value);
+  }
+}
+
+calculateButton.addEventListener("click", calculate);
+
+function calculate() {
+  firstOperandValue = parseInt(memoryDisplay.innerText);
+  secondOperandValue = parseInt(displayValue.innerText);
+  memoryDisplay.innerText = "";
+  operationDisplay.innerText = "";
+
+  switch (mathOperator) {
+    case "+":
+      resultValue = firstOperandValue + secondOperandValue;
+      displayValue.innerText = resultValue;
+      break;
+    case "-":
+      resultValue = firstOperandValue - secondOperandValue;
+      displayValue.innerText = resultValue;
+      break;
+    case "*":
+      resultValue = firstOperandValue * secondOperandValue;
+      displayValue.innerText = resultValue;
+      break;
+    case "/":
+      resultValue = firstOperandValue / secondOperandValue;
+      displayValue.innerText = resultValue;
+      break;
+    case "**":
+      resultValue = firstOperandValue ** secondOperandValue;
+      displayValue.innerText = resultValue;
+      break;
+    default:
+      resultValue = secondOperandValue;
   }
 }
 
@@ -54,18 +90,7 @@ function clearAll(e) {
   if (e.target.classList.contains("clear-all")) {
     displayValue.innerText = "";
     memoryDisplay.innerText = "";
+    operationDisplay.innerText = "";
     console.log(e.target.value);
   }
 }
-
-// } else if (e.target.classList.contains("decimal-point")) {
-//   let display = e.target.value;
-//   displayValue.innerText += display;
-//   console.log(e.target.value, "decimal-point");
-// } else if (e.target.classList.contains("operator")) {
-//   console.log(e.target.value, "operator");
-// } else if (e.target.classList.contains("clear-entry")) {
-//   console.log("clear-entry");
-// } else if (e.target.classList.contains("clear-all")) {
-//   console.log("clear-all");
-// }
